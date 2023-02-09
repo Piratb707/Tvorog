a832200987a3d1a8dd6e54210afb0d60bc3aa67b
Your Name <you@example.com>
2023-02-09 18:24:23 -0300

    add new features

--- keyboard.py
+++ keyboard.py
@@ -1,8 +1,10 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove,ReplyKeyboardMarkup,KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 b1 = KeyboardButton('/help')
 b2 = KeyboardButton('/desc')
 b3 = KeyboardButton('/img')
 kb.insert(b1).insert(b2).insert(b3)
+
+ikb = InlineKeyboardMarkup(row=2)
+ib = InlineKeyboardMarkup()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove,ReplyKeyboardMarkup,KeyboardButton
+from aiogram.types import ReplyKeyboardRemove
 
 from config import TOKEN
 from Tex_Mes import *
@@ -7,6 +7,9 @@ from keyboard import *
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
+
+async def on_startup(_):
+    print("Starting !")
 
 @dp.message_handler(commands=['start'])
 async def start_cmd(message: types.Message):
@@ -16,14 +19,22 @@ async def start_cmd(message: types.Message):
                            reply_markup=kb)
     await message.delete()
 
+@dp.message_handler(commands=['comments'])
+async def comments_cmd(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id,
+                           text=COMMENTS,
+                           parse_mode='HTML'
+                          )
+    await message.delete()
+
 @dp.message_handler(commands=['help'])
 async def help_cmd(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
                            text=HELP_COMMAND, parse_mode='HTML',
-                           reply_markup = ReplyKeyboardRemove())
+                           reply_markup=ReplyKeyboardRemove())
     await message.delete()
 
-@dp.message_handler(commands=['desc'])
+@dp.message_handler(commands=['about'])
 async def help_cmd(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
                            text=DESCRIPTION, parse_mode='HTML')
@@ -36,4 +47,4 @@ async def send_image(message: types.Message):
     await message.delete()
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
